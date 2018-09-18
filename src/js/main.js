b0541cfc83fdcda1b900f9b692e51c626eb47bf5
Killian Grant <killian@killiangrant.com>
2018-09-18 16:27:14 -0600

    set up webpack and create main files

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+require("../scss/main.scss");
+
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/js/main.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "main.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Tiny Solar System",
+      filename: "index.html",
+      template: "./src/html/index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  }
+};
